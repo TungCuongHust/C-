2a0d33d16484c815df35b4fcc2ef21a49e0c1cdb
tungcuong <tungcuong1997@gmail.com>
2020-09-16 00:24:39 +0700

    [Learn] relearn C++

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # C-
+
+Commandline to compile C++ programming is:
+
+```
+ g++ -Wall -ansi -o abc abc.cpp
+```
+Next, run abc program:
+```
+./abc
+```
---
+++ basic/1_helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+// '#' is preprocessor directive, prefix "pre" is which run before compile.
+using namespace std;
+int main(void)
+{
+    cout << "Day la chuong trinh C++ day tien sau bao nhieu nam gap lai!";
+    return 0;
+}
